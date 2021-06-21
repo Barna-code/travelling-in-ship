457583a20e63330a931c68d1752a4feef5147f99
Shubham392007 <85775945+Shubham392007@users.noreply.github.com>
2021-06-21 11:08:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var sea;
+var ship;
+var shipImage;
+var seaImage;
+
+function preload(){
+  shipImage=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4,png")
+  seaImage=loadImage("sea.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+ 
+  sea=createSprite(0,0);
+  sea.addImage(seaImage)
+  sea.velocityX=-1
+ 
+  ship=createSprite(200,207,99,35,)
+ ship.addAnimation("movingship",shipImage) 
+ ship.scale=0.3
+ 
+ 
+}
+
+function draw() {
+  background("blue");
+  
+
+if(sea.x<0){
+  sea.x=sea.width/4
+}
+
+  text("SHUBHAM",40,80);
+  drawSprites();
+}
